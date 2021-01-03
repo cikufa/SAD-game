00d5db9d853f549ad2adc10f32b9283bc174b961
anonymous <fateme.rmg@gmail.com>
2021-01-03 20:22:08 +0330

    SFX

---
+++ Assets/Scripts/Managers/ItemSFX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSFX : MonoBehaviour
+{
+    AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();   
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            source.Play();
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && source.isPlaying)
+        {
+            source.Stop();
+        }
+    }
+}
