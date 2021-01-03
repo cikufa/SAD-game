853869230210a42ca32417bd299a6ec023bb767b
sarah <sarah77.amini@gmail.com>
2021-01-03 15:42:21 -0800

    pro

---
+++ Assets/Scripts/UI/BlackAnim.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator generalAnim;
+
+    public void AnimationEnded()
+    {
+        generalAnim.SetTrigger("BlackStart");
+    }
+}
