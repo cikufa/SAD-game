f2fc5fc0aae2a04e89e3db0d3bd8de9c2135df0f
sarah <sarah77.amini@gmail.com>
2021-01-20 09:17:15 -0800

    reset follow

---
+++ Assets/Scripts/ResetFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetFollow : MonoBehaviour
+{
+    public GameObject door1, door2, door3;
+
+    public GameObject FollowEnemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+            door1.GetComponent<SpriteRenderer>().enabled = false;
+            door2.GetComponent<SpriteRenderer>().enabled = false;
+            door3.GetComponent<SpriteRenderer>().enabled = false;
+            FollowEnemy.SetActive(false);
+            FollowEnemy.GetComponent<Transform>().position = new Vector3(390, -78, 0);
+            door1.GetComponent<BoxCollider2D>().isTrigger = true;
+            door2.GetComponent<BoxCollider2D>().isTrigger = true;
+            door3.GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+    }
+}
